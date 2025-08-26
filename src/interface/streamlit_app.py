122ca2b13c90508429f4664d52b1f84ea8bcbad1
Mohamed Cherif <innerloopinc@gmail.com>
2025-08-26 23:02:29 +0100

    chore(ui): tweak Streamlit app logs

--- src/interface/streamlit_app.py
+++ src/interface/streamlit_app.py
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
-=======
 import streamlit as st
 
 DEBUG_MINIMAL = False  # Set to False to run the full app
@@ -59,7 +17,7 @@ else:
     # Model directory
     MODEL_DIR = "models/vit-finetuned"
 
-    st.title("Plant Disease Classifier")
+    st.title("Plant Disease LLM Assistant")
     st.write("App is starting...")
     st.write("b4 uploaded_file")
     uploaded_file = st.file_uploader(
@@ -99,4 +57,3 @@ else:
 
         st.markdown(f"**Prediction:** {pred_class}")
         st.markdown(f"**Confidence:** {confidence:.2f}")
->>>>>>> e886e3e (Initial scaffold for Plant Disease LLM Assistant, the purpose is to extend my previous Plant Disease Classifier Repo with retrieval-augmented generation and LLMs to create a plant health assistant)
